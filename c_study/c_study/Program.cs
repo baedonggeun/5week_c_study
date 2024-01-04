18b7853d51d19c9f8953e0b3266ebd70f5abf14b
baedonggeun <hispanie3@gmail.com>
2024-01-04 10:07:25 +0900

    기본 구조 설명
    주석 작성

--- c_study/c_study/Program.cs
+++ c_study/c_study/Program.cs
@@ -5,6 +5,37 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("자동 완성 연습");
+
+            /*
+             * 
+             * 
+            //c# 에서 기본적으로 제공하는 namespace 를 사용하기 위해 참조해야하는 코드
+            //console 클래스를 사용하기 위해 필요 
+            using System;
+            
+            //클래스 및 기타 namespace의 컨테이너
+            namespace HelloWorld
+            //
+            {
+                //c#클래스를 정의하는 키워드
+                //클래스명 = program
+                class program
+                {
+                    //c#의 진입점(entry point)
+                    //main 메서드는 프로그램이 시작할 때 자동으로 호출되는 메서드로 필수
+                    static void Main(string[] args)
+                    {
+                        //콘솔에 출력할 내용을 지정하는 코드
+                        Console.WriteLine("Hello World!");
+                    }
+                }
+            }
+             * 
+             * 
+             */
+
         }
     }
 }
